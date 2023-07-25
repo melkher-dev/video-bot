094fc8f7cfdb35eb691e8da30152ccb42987b27f
melkherv <camelotvm48@gmail.com>
2023-07-25 14:24:51 +0300

    admin middleware

---
+++ app/Http/Middleware/Admin.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class Admin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (auth()->check() && auth()->user()->is_admin) {
+            return $next($request);
+        }
+
+        //return error message
+        abort(403, 'Unauthorized action.');
+        // return redirect()->route('home')->with('error', 'You are not authorized to access this page.');
+    }
+}
